90f63a968f78c055e492b97789c4d164a20b72e2
r patterson <r.patterson142@googlemail.com>
2020-08-13 12:11:59 +0100

    New Changes

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,15 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow, QPushButton
+import pyautogui
 
 
 
 import sys
 
 def say_hello():
- print("Button clicked, Hello!")
+    screenWidth, screenHeight = pyautogui.size()
+    print("Button clicked, Hello!", screenWidth, screenHeight)
+    im = pyautogui.screenshot(region=(0,0, 300, 400))
 
 def main():
 
@@ -19,6 +22,7 @@ def main():
     button = QPushButton("Download")
     button.resize(250, 150)
     button.clicked.connect(say_hello)
+
     button.show()
 
 
