1461e55f6b4380f43bc36de4fb7ffd038fba04ac
r patterson <r.patterson142@googlemail.com>
2020-08-16 12:43:07 +0100

    Proper use of inheritence, Less dependencies, Legible coding style - Complete Refresh

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,36 +1,55 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow, QPushButton
-import pyautogui
-
-
-
 import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QToolBar, QAction
 
-def say_hello():
-    screenWidth, screenHeight = pyautogui.size()
-    print("Button clicked, Hello!", screenWidth, screenHeight)
-    im = pyautogui.screenshot(region=(0,0, 300, 400))
+#QTLayouts? For more widgets/arranging...
+#We'll need this simply for the buttons...
 
-def main():
+class MainWindow(QMainWindow):
 
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("SnipPy")
 
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    # window = QMainWindow()
-    # window.resize(250, 150)
-    # window.show()
+        label = QLabel("THIS IS nice!!")
+        label.setAlignment(Qt.AlignCenter)
+        
+        self.setCentralWidget(label)
+        
+        #Toolbar created for tidyness. 
+        toolbar = QToolBar("My main toolbar")
+        self.addToolBar(toolbar)
 
-    button = QPushButton("Download")
-    button.resize(250, 150)
-    button.clicked.connect(say_hello)
+        button_action = QAction("Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        toolbar.addAction(button_action)
 
-    button.show()
+    def onMyToolBarButtonClick(self, s):
+        print("click", s)
+        #Get everything out of the way...
+        self.hide()
 
+app = QApplication(sys.argv)
 
+window = MainWindow()
+window.show() # IMPORTANT!!!!! Windows are hidden by default.
 
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+#Remember... Main windows can have docking, qWidgets can be seperate.
+#We want docking
 
+# Start the event loop.
+app.exec_()
 
+# from fbs_runtime.application_context.PyQt5 import ApplicationContext
+# from PyQt5.QtWidgets import QMainWindow
 
-if __name__ == '__main__':
-    main()
+# import sys
+
+# if __name__ == '__main__':
+#     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+#     window = QMainWindow()
+#     window.resize(250, 150)
+#     window.show()
+#     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+#     sys.exit(exit_code)
